32c35493dac6a20011182127308110eaa354dfe7
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-10-08 16:16:36 +0100

    more debug

--- R/run-region-updates.R
+++ R/run-region-updates.R
@@ -373,7 +373,10 @@ if (sys.nframe() == 0) {
   args <- rru_cli_interface()
   setup_log_from_args(args)
   if (args$datadir) {
+    futile.logger::flog.debug("running with results output to data/results")
     DATA_DIR <- "data/results"
+  }else{
+    futile.logger::flog.trace("outputting results to root folder")
   }
   # Pull in the definition of the datasets after we have had the chance to
   source(here::here("R", "dataset-list.R"))
